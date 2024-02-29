80fa0fff3ad3adab4a8e15dd3cbc133832fb10fd
nakamurau1 <fyuichi@gmail.com>
2024-02-29 20:23:17 +0900

    wip

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,6 +4,7 @@ use std::fmt;
 pub enum Token {
     Integer(i64),
     Float(f64),
+    String(String),
     Symbol(String),
     LParen,
     RParen,
@@ -14,6 +15,7 @@ impl fmt::Display for Token {
         match self {
             Token::Integer(n) => write!(f, "{}", n),
             Token::Float(n) => write!(f, "{}", n),
+            Token::String(s) => write!(f, "{}", s),
             Token::Symbol(s) => write!(f, "{}", s),
             Token::LParen => write!(f, "("),
             Token::RParen => write!(f, ")"),
@@ -23,36 +25,69 @@ impl fmt::Display for Token {
 
 #[derive(Debug)]
 pub struct TokenError {
-    ch: char,
+    err: String,
 }
 
 impl fmt::Display for TokenError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Unexpected character: {}", self.ch)
+        write!(f, "Tokenization error: {}", self.err)
     }
 }
 
-pub fn tokenize(program: &str) -> Result<Vec<Token>, TokenError> {
-    let program2 = program.replace("(", " ( ").replace(")", " ) ");
-    let words = program2.split_whitespace();
-    let mut tokens: Vec<Token> = Vec::new();
-    for word in words {
-        match word {
-            "(" => tokens.push(Token::LParen),
-            ")" => tokens.push(Token::RParen),
+pub fn tokenize(input: &str) -> Result<Vec<Token>, TokenError> {
+    let mut tokens = Vec::new();
+    let mut chars = input.chars().collect::<Vec<char>>();
+
+    if chars.is_empty() {
+        return Ok(tokens);
+    }
+
+    while !chars.is_empty() {
+        let mut ch = chars.remove(0);
+        match ch {
+            '(' => tokens.push(Token::LParen),
+            ')' => tokens.push(Token::RParen),
+            '"' => {
+                let mut word = String::new();
+                while !chars.is_empty() && chars[0] != '"' {
+                    word.push(chars.remove(0));
+                }
+
+                if !chars.is_empty() && chars[0] == '"' {
+                    chars.remove(0);
+                } else {
+                    return Err(TokenError {
+                        err: format!("Unterminated string: {}", word),
+                    });
+                }
+
+                tokens.push(Token::String(word));
+            }
             _ => {
+                let mut word = String::new();
+                while !chars.is_empty() && !ch.is_whitespace() && ch != '(' && ch != ')' {
+                    word.push(ch);
+                    let peek = chars[0];
+                    if peek == '(' || peek == ')' {
+                        break;
+                    }
+
+                    ch = chars.remove(0);
+                }
+
                 if !word.is_empty() {
                     tokens.push(if let Ok(i) = word.parse::<i64>() {
                         Token::Integer(i)
                     } else if let Ok(f) = word.parse::<f64>() {
                         Token::Float(f)
                     } else {
-                        Token::Symbol(word.to_string())
-                    })
+                        Token::Symbol(word)
+                    });
                 }
             }
         }
     }
+
     Ok(tokens)
 }
 
@@ -82,6 +117,7 @@ mod tests {
         (
             (define r 10)
             (define pi {})
+            (define str \"hogehoge\")
             (* pi (* r r))
         )
         ",
@@ -102,6 +138,11 @@ mod tests {
                 Token::Symbol("define".to_string()),
                 Token::Symbol("pi".to_string()),
                 Token::Float(std::f64::consts::PI),
+                Token::RParen,
+                Token::LParen,
+                Token::Symbol("define".to_string()),
+                Token::Symbol("str".to_string()),
+                Token::String("hogehoge".to_string()),
                 Token::RParen,
                 Token::LParen,
                 Token::Symbol("*".to_string()),
